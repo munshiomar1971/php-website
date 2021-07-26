6a3edfe8769f58e06ec827117993f86c87eb4b35
worstfrontendcoder <omarsumon1030@gmail.com>
2021-07-25 20:13:16 -0500

    initial commit

---
+++ website/index.php
@@ -0,0 +1,32 @@
+<!-- Created by Omar Sumon on Sunday July 25, 2021 to master php by making a website with it -->
+<!-- this is a semi portfolio -->
+
+<?php 
+    // created 8:09
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div id="bio">
+            <?php echo "hello, world"; ?>
+        </div>
+    </main>
+</body>
+</html>
