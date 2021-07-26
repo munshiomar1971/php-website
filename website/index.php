96b30701082805449c8a11d83daa0ef8e24e3ad5
worstfrontendcoder <omarsumon1030@gmail.com>
2021-07-26 10:14:37 -0500

    bio almost done

--- website/index.php
+++ website/index.php
@@ -60,6 +60,21 @@
         <br><br><br><br><br>
         <div id="bio">
             <h1>PHP Developer</h1>
+            <hr>
+            <br>
+            <p>
+            The leather jacked showed the scars of being his favorite for years.
+             It wore those scars with pride,
+              feeling that they enhanced his presence rather than diminishing it. 
+              The scars gave it character and had not overwhelmed to the point that it had become ratty.
+               The jacket was in its prime and it knew it.
+               There was something in the tree. It was difficult to tell from the ground, but Rachael could see movement. She squinted her eyes and peered in the direction of the movement, trying to decipher exactly what she had spied. The more she peered, however, the more she thought it might be a figment of her imagination. Nothing seemed to move until the moment she began to take her eyes off the tree. Then in the corner of her eye, she would see the movement again and begin the process of staring again.
+            </p>
+            <p>
+            The young man wanted a role model. He looked long and hard in his youth, but that role model never materialized. His only choice was to embrace all the people in his life he didn't want to be like.
+It was difficult for him to admit he was wrong. He had been so certain that he was correct and the deeply held belief could never be shaken. Yet the proof that he had been incorrect stood right before his eyes. "See daddy, I told you that they are real!" his daughter excitedly proclaimed.
+            </p>
+            
         </div>
     </main>
 </body>
